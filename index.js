e1e86c5ae81ebee87fca8904171e00e7906ae3d1
Micah Lynn <micah@sabado.com>
2020-01-24 15:14:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function dec(n) {
 
 function makeInt(n) {
   if (n === (a.toString()))
-    return parseInt(a.toString())
+    return parseInt(a.toString(), 10)
 }
 
 function preserveDecimal(n) {
